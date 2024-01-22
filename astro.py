462ebd4319fd4c01395a0bc3535b8e8db3b3e556
Nicholas Zubrzycki <NicholasZubrzycki02@gmail.com>
2024-01-21 20:05:11 -0500

    Removed print and finished general structure. Add .gitignore

--- astro.py
+++ astro.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import date
 
 #Fixed latitude and longitude for Newark.
 URL = "https://www.7timer.info/bin/api.pl?"
@@ -30,6 +31,7 @@ def quality(x):
 #The range and added values change depending on the hour the program runs because of the API.
 #Add a value of 7 when done around 3:00, add a value of 1 when done around 6:00. Switch AM/PM.
 message = ""
+worthSeeing = False
 for data in response['dataseries'][:5]:
     time = data['timepoint'] + 1 % 12
     if(time < 12):
@@ -40,11 +42,10 @@ for data in response['dataseries'][:5]:
         message += f"{time-12} AM: "
     
     cloudCover = data['cloudcover']
-    if(cloudCover < 5):
+    if(cloudCover <= 5):
         message += f"Up to {cloudCover}0% cloud coverage, "
+        worthSeeing = True
     else:
         message += f"Up to {cloudCover+1}0% cloud coverage, "
 
-    message += f"{quality(data['seeing'])} seeing, {quality(data['transparency'])} transparency.\n"
-
-print(message)
+    message += f"{quality(data['transparency'])} transparency, {quality(data['seeing'])} seeing.\n\n"
