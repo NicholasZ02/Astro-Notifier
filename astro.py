b102c856df978e53191494bbf5004ca8d05ee404
Nicholas Zubrzycki <NicholasZubrzycki02@gmail.com>
2024-01-22 23:25:18 -0500

    Custom lat/lon in config. Rough outline for sending image working.

--- astro.py
+++ astro.py
@@ -1,19 +1,20 @@
 import requests
 import config
+import os
 from datetime import date
 
-#Fixed latitude and longitude for Newark.
+#Latitude and Longitude come from configuation file.
 URL = "https://www.7timer.info/bin/api.pl?"
 params = {
-    "lat": 40.7355,
-    "lon": -74.1741,
+    "lat": config.LAT,
+    "lon": config.LON,
     "product": 'astro',
     "unit": 'british',
     "output": 'json',
 }
 
 try:
-    response = requests.get(URL, params).json()
+    weatherResponse = requests.get(url=URL, params=params).json()
 except:
     quit()
 
@@ -30,10 +31,11 @@ def quality(x):
 
 #Accessing the first few entries which relate to the current and next day over a period of 12 hours.
 #The range and added values change depending on the hour the program runs because of the API.
-#Add a value of 7 when done around 3:00, add a value of 1 when done around 6:00. Switch AM/PM.
+#Add a value of 7 when done around 3:00, add a value of 1 when done around 6:00, add a value of 19 at 11:00.
+#Switch AM/PM when done in AM or PM.
 message = ""
 goodClouds = False
-for data in response['dataseries'][:5]:
+for data in weatherResponse['dataseries'][:5]:
     time = data['timepoint'] + 1 % 12
     if(time < 12):
         message += f"{time} PM: "
@@ -52,11 +54,26 @@ for data in response['dataseries'][:5]:
     message += f"{quality(data['transparency'])} transparency, {quality(data['seeing'])} seeing.\n\n"
 
 #Pushover API Notifier. Only send a message under ideal conditions.
-if(goodClouds):
-    url = "https://api.pushover.net/1/messages.json"
+if(True):
+    pushoverURL = "https://api.pushover.net/1/messages.json"
+    imageURL = f"https://www.7timer.info/bin/astro.php?lon={config.LON}&lat={config.LAT}&lang=en&ac=0&unit=metric&output=internal&tzshift=0"
+    try:
+        imageResponse = requests.get(imageURL)
+        with open("Forecast.jpg", "wb") as f:
+            f.write(imageResponse.content)
+    except:
+        pass
     title = f"Forecast for {date.today().strftime('%m/%d/%y')}"
-    pushoverRequest = {"token": config.APPLICATION_TOKEN,
-                        "user": config.USER_TOKEN,
-                        "title": title,
-                        "message": message}
-    requests.post(url, pushoverRequest)
+    pushoverData = {"token": config.APPLICATION_TOKEN,
+                    "user": config.USER_TOKEN,
+                    "title": title,
+                    "message": message}
+    try:
+        pushoverImage={"attachment": open("Forecast.jpg","rb")}
+        requests.post(url=pushoverURL, data=pushoverData, files=pushoverImage)
+        os.remove("Forecast.jpg")
+    except:
+        try:
+            requests.post(url=pushoverURL, data=pushoverData)
+        except:
+            quit()
